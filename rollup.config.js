13561e3ba07482b3330c073ede18adc385d3a87c
Brett Zamir <brettz9@yahoo.com>
2021-06-22 17:21:18 +0800

    Refactor: Align with espree re: ESM

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,11 @@
 export default {
     input: "./lib/index.js",
     external: ["fs"],
+    treeshake: false,
     output: {
         exports: "default",
         format: "cjs",
-        file: "dist/eslint-visitor-keys.cjs"
+        file: "dist/eslint-visitor-keys.cjs",
+        sourcemap: true
     }
 };
