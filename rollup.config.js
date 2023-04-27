8f12e39c3032489739d3780131ac09764d5e8c3d
Brad Zacher <brad.zacher@gmail.com>
2023-04-27 09:32:49 +0930

    fix: correct types for node16 resolution

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,21 @@
-export default {
-    input: "./lib/index.js",
-    treeshake: false,
-    output: {
-        format: "cjs",
-        file: "dist/eslint-visitor-keys.cjs"
+import dts from "rollup-plugin-dts";
+
+export default [
+    {
+        input: "./lib/index.js",
+        treeshake: false,
+        output: {
+            format: "cjs",
+            file: "dist/eslint-visitor-keys.cjs"
+        }
+    },
+    {
+        plugins: [dts()],
+        input: "./lib/index.js",
+        treeshake: false,
+        output: {
+            format: "cjs",
+            file: "dist/eslint-visitor-keys.d.cts"
+        }
     }
-};
+];
